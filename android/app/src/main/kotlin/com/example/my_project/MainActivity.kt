7b3a1cfc20d09d196fe5e94dcbe88625bf713c04
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-02 11:57:16 +0000

    Updating to latest FlutterFlow output.

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.settings
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/index.dart
@@ -0,0 +1,3 @@
+// Export pages
+export '/auth2/auth2_widget.dart' show Auth2Widget;
+export '/profile04/profile04_widget.dart' show Profile04Widget;
